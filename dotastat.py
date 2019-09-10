6f9f3850824c4092dd5c9b2b78a651a7831c9204
Merodoc <rowan@overworld.net.au>
2019-09-10 12:33:31 +1000

    updates to dotastat.py

--- dotastat.py
+++ dotastat.py
@@ -7,7 +7,7 @@ Created on Thu Sep  5 11:44:15 2019
 """
 
 import csv
-
+import numpy as np
 
 Stats = []
 with open('TI9.csv', newline = '') as csvfile:
@@ -19,10 +19,27 @@ Team1 = []
 Team2 = []
 Winner = []
 i = 0
-for game in Stats:       
-    Team1.append(Stats[i][1])
-    Team2.append(Stats[i][4])
-    Winner.append(Stats[i][7])
-    i += 1
+for game in Stats:
+    if i == 0:
+        i += 1
+    else:
+        Team1.append(Stats[i][1])
+        Team2.append(Stats[i][4])
+        Winner.append(Stats[i][7])
+        i += 1       
     
 Teamlist = ['OG','Team Liquid', 'PSG.LGD','Team Secret','Vici Gaming','Evil Geniuses','Royal Never Give Up','Infamous Gaming', 'Newbee', 'TNC Predator','Virtus.pro','Fnatic','Alliance','Mineski','Natus Vincere', 'Chaos Esports Club', 'Keen Gaming', 'Ninjas in Pyjamas']
+
+
+wij = np.zeros((len(Teamlist), len(Teamlist)))
+
+wincounts = []
+for x in Teamlist:
+    print(x)
+    wincounts.append(Winner.count(x))
+    
+            
+print(wincounts)
+
+print(Team1)
+
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 11:53:56 2019
+
+@author: merodoc
+"""
+
+        import numpy as np
